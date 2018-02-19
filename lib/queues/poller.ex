3fbb84d7f9d4b3e685a43ff6f3d08aad81543c39
Jusabe Guedes <jusabe.09@gmail.com>
2018-02-18 21:22:53 -0300

    refactored poller to use :queue

--- lib/queues/poller.ex
+++ lib/queues/poller.ex
@@ -8,42 +8,68 @@ defmodule Octoconf.Queues.Poller do
   end
 
   def init(queue) do
-    {:producer, %{queue: queue, events: [], pending_demand: 0}}
+    {:producer, %{queue: queue, events: :queue.new, pending_demand: 0}}
+  end
+
+  def poll(queue) do
+    queue
+    |> via_tuple
+    |> GenStage.cast(:poll)
   end
 
   def handle_demand(demand, state) do
     Logger.debug "asked for #{demand} messages"
-    GenStage.cast(via_tuple(state.queue), :check_for_messages)
-    {:noreply, [], %{state | pending_demand: demand + state.pending_demand}}
-  end
-
-  def handle_cast(:check_for_messages, %{pending_demand: demand, events: events} = state) when demand <= length(events) do
-    dispatch_events(events, state)
-  end
-
-  def handle_cast(:check_for_messages, state) do
-    events = state.events ++ SQS.receive_message!(state.queue)
-    dispatch_events(events, state)
-  end
-
-  def dispatch_events(events, %{pending_demand: demand} = state) when demand == 0 or events == [] do
-    GenStage.cast(via_tuple(state.queue), :check_for_messages)
+    poll(state.queue)
+    state = %{state | pending_demand: demand + state.pending_demand}
     {:noreply, [], state}
   end
 
-  def dispatch_events(events, state) do
-    {to_dispatch, remaining} = Enum.split(events, state.pending_demand)
-    state = %{state | events: remaining, pending_demand: state.pending_demand - length(to_dispatch)}
-    GenStage.cast(via_tuple(state.queue), :check_for_messages)
-    Logger.debug "dispatched messages #{inspect(to_dispatch)}"
-    {:noreply, to_dispatch, state}
+  def handle_cast(:poll, %{pending_demand: 0} = state) do
+    poll(state.queue)
+    {:noreply, [], state}
   end
 
+  def handle_cast(:poll, state) do
+    poll(state.queue)
+    events = 
+      SQS.receive_message!(state.queue)
+      |> Enum.reduce(state.events, fn msg, acc -> 
+        :queue.in(msg, acc)
+      end)
+    dispatch_events(%{state | events: events})
+  end
+
+  def dispatch_events(state, to_dispatch \\ [])
+
+  def dispatch_events(%{pending_demand: 0} = state, to_dispatch) do
+    do_dispatch_events(state, to_dispatch)
+  end
+
+  def dispatch_events(state, to_dispatch) do
+    case :queue.out(state.events) do
+      {{:value, event}, events} ->
+        state = %{state | events: events, pending_demand: state.pending_demand - 1}
+        dispatch_events(state, [event | to_dispatch])
+      
+      {:empty, events} ->
+        state = %{state | events: events}
+        do_dispatch_events(state, to_dispatch)
+    end
+  end
+
+  defp do_dispatch_events(state, to_dispatch) do
+    Logger.debug "dispatched messages #{inspect(to_dispatch)}"
+    {:noreply, Enum.reverse(to_dispatch), state}
+  end
+
+  # This will prevent unexpected crashes when somebody
+  # sends an unhandled info
   def handle_info(_msg, state) do
     {:noreply, [], state}
   end
 
   def via_tuple(queue) do
-    Octoconf.Registry.via_tuple({__MODULE__, queue})
+    {__MODULE__, queue}
+    |> Octoconf.Registry.via_tuple
   end
 end 
